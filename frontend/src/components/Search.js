cb3ca267fb06c25429ff30ad75073be08c71fe9e
David Portilla <94.david.pm@gmail.com>
2024-09-04 13:49:34 -0600

    state handling for search

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Container, Col, Row, Button, Form} from 'react-bootstrap';
 
-const Search = () => {
+const Search = (props) => {
     return(<>
         <Container className="mt-4">
             <Row className="justify-content-center">
                 <Col xs={12} md={8} lg={6}>
-                    <Form>
+                    <Form onSubmit={props.handleSubmit}>
                         <Row>
                             <Col xs={9}>
-                                <Form.Control placeholder="First name" />
+                                <Form.Control
+                                    type="text"
+                                    value={props.word}
+                                    onChange={(e) => props.setWord(e.target.value)}
+                                    placeholder="Search for image..."
+                                />
                             </Col>
                             <Col xs={3}>
                                 <Button variant="primary" type="submit">Search</Button>
