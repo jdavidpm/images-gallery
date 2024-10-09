b56d3f4e6ba1a4b1755157fa2c43d9e8e449aba5
David Portilla <94.david.pm@gmail.com>
2024-10-09 12:37:24 -0600

    new image endpoint added

--- api/main.py
+++ api/main.py
@@ -1,11 +1,33 @@
-# save this as app.py
-from flask import Flask
+import requests 
+import json
+from flask import Flask, request
+
+with open('/etc/config_images_gallery.json') as config_file:
+    config = json.load(config_file)
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = config["UNSPLASH_KEY"]
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Please create config file config_images_gallery and add UNSPLASH_KEY")
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!!"
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    
+    headers = {
+        "Accept-Version": "v1",
+        "Authorization": f"Client-ID {UNSPLASH_KEY}"
+    }
+    params = {
+        "query": word
+    }
+    
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
