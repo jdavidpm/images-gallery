af11064d2794ca1330faf3568577d27dd774febd
David Portilla <94.david.pm@gmail.com>
2024-10-16 14:33:25 -0600

    format changes

--- api/main.py
+++ api/main.py
@@ -1,9 +1,9 @@
-import requests 
 import json
+import requests
 from flask import Flask, request
 from flask_cors import CORS
 
-with open('/etc/config_images_gallery.json') as config_file:
+with open("/etc/config_images_gallery.json", encoding="utf-8") as config_file:
     config = json.load(config_file)
 
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
@@ -17,21 +17,18 @@ CORS(app)
 
 app.config["DEBUG"] = bool(config["DEBUG"])
 
+
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    
-    headers = {
-        "Accept-Version": "v1",
-        "Authorization": f"Client-ID {UNSPLASH_KEY}"
-    }
-    params = {
-        "query": word
-    }
-    
-    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+
+    headers = {"Accept-Version": "v1", "Authorization": f"Client-ID {UNSPLASH_KEY}"}
+    params = {"query": word}
+
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params, timeout=10)
     data = response.json()
     return data
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
