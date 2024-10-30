fb152d7da30c0977c0b903278836aafa7f24ad8a
David Portilla <97.david.pm@gmail.com>
2024-10-30 20:47:12 +0000

    basic endpoint for images post and get

--- api/main.py
+++ api/main.py
@@ -2,7 +2,10 @@ import json
 import requests
 from flask import Flask, request
 from flask_cors import CORS
-from mongo_client import insert_test_document
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 with open("/etc/config_images_gallery.json", encoding="utf-8") as config_file:
     config = json.load(config_file)
@@ -18,8 +21,6 @@ CORS(app)
 
 app.config["DEBUG"] = bool(config["DEBUG"])
 
-insert_test_document()
-
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
@@ -31,6 +32,15 @@ def new_image():
     data = response.json()
     return data
 
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        return [image for image in images_collection.find({})]
+    if request.method == "POST":
+        image_req = request.get_json()
+        image_req["_id"] = image_req.get("id")
+        new_image = images_collection.insert_one(image_req)
+        return {"inserted_id": new_image.inserted_id}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
